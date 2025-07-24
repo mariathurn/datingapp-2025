aa1877dd3c1fecf7275cfd8c023a4ff65ece360a
Maria Thurn <190928180+mariathurn@users.noreply.github.com>
2025-07-24 12:14:13 +0200

    Token

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
 using System.Runtime.CompilerServices;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenServices>();
 
 var app = builder.Build();
 
